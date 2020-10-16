1614f28370c9bf0ad68718a288f7c88f3a81a81e
Leo <eta.development.leo@outlook.com>
2020-10-16 10:39:40 +0300

    Prepare for deploy

--- src/api/mobile-devices.js
+++ src/api/mobile-devices.js
@@ -5,17 +5,7 @@ export function getMobileDevices () {
         method: 'GET',
         headers: {
             Accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiZXRhLm9wZXIubGVvIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiTU9CSUxFX0RFVklDRV9VU0VSIiwibmJmIjoxNjAyNjYzOTg2LCJleHAiOjE2MDI4NDM5ODYsImlzcyI6IkVuZXJneVRlY2hBdWRpdC5Nb2JpbGVUcmFja2VyLldlYiIsImF1ZCI6IkVuZXJneVRlY2hBdWRpdC5Nb2JpbGVUcmFja2VyLkNsaWVudCJ9.Cm2vm9on5r1fAZetyPmkdwZck2Xqm8MOHDoEcPNIkzQ'
-        },
-    }).then(response => response.json());
-}
-
-export function getTimelines (mobileDeviceId, workDate) {
-    return fetch(`${ appConstants.routes.host }${ appConstants.routes.timeline }?mobileDeviceId=${ mobileDeviceId }&workDate=${ new Date(workDate).toISOString() }`, {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiZXRhLm9wZXIubGVvIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiTU9CSUxFX0RFVklDRV9VU0VSIiwibmJmIjoxNjAyNjYzOTg2LCJleHAiOjE2MDI4NDM5ODYsImlzcyI6IkVuZXJneVRlY2hBdWRpdC5Nb2JpbGVUcmFja2VyLldlYiIsImF1ZCI6IkVuZXJneVRlY2hBdWRpdC5Nb2JpbGVUcmFja2VyLkNsaWVudCJ9.Cm2vm9on5r1fAZetyPmkdwZck2Xqm8MOHDoEcPNIkzQ'
+            Authorization: `Bearer ${ appConstants.testToken }`
         },
     }).then(response => response.json());
 }
@@ -25,14 +15,13 @@ export async function getTimelinesAsync (mobileDeviceId, workDate) {
         method: 'GET',
         headers: {
             Accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiZXRhLm9wZXIubGVvIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiTU9CSUxFX0RFVklDRV9VU0VSIiwibmJmIjoxNjAyNjYzOTg2LCJleHAiOjE2MDI4NDM5ODYsImlzcyI6IkVuZXJneVRlY2hBdWRpdC5Nb2JpbGVUcmFja2VyLldlYiIsImF1ZCI6IkVuZXJneVRlY2hBdWRpdC5Nb2JpbGVUcmFja2VyLkNsaWVudCJ9.Cm2vm9on5r1fAZetyPmkdwZck2Xqm8MOHDoEcPNIkzQ'
+            Authorization: `Bearer ${ appConstants.testToken }`
         },
     }).catch((error) => {
         return Promise.reject(error);
     });
 
-    return  await response.json().catch((error) => {
+    return await response.json().catch((error) => {
         return Promise.reject(error);
     });
-
 }
