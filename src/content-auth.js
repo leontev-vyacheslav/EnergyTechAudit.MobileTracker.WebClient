3c5ad900dc2635443a3f493045b776b8f41a9ce4
Leo <eta.development.leo@outlook.com>
2021-06-10 14:44:31 +0300

    Fix routing

--- src/content-auth.js
+++ src/content-auth.js
@@ -1,32 +1,30 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import routes from './constants/app-routes';
 import { SideNavOuterToolbar as SideNavBarLayout } from './layouts';
 import { Footer } from './components';
 import AppConstants from './constants/app-constants';
-import Loader from './components/loader/loader';
 
-const RegistrationPage = lazy(() => import('./pages/registration/registration'));
+
+import RegistrationPage from './pages/registration/registration';
 
 const ContentAuth = () => {
     return (
         <>
             <SideNavBarLayout title={ AppConstants.appInfo.title }>
                 <Switch>
-                    <Suspense fallback={ <Loader/> }>
-                        { routes.map(({ path, component }) => (
-                            <Route exact key={ path } path={ path } component={ component }/>
-                        )) }
-                        <Route exact={ false } key={ '/confirm-registration' } path={ '/confirm-registration' } >
-                            <RegistrationPage />
-                        </Route>
-                        <Route exact={ false } key={ '/reject-registration' }  path={ '/reject-registration' } >
-                            <RegistrationPage />
-                        </Route>
-                        <Route path={ '*' }>
-                            <Redirect from={ '/' } to={ '/home' } />
-                        </Route>
-                    </Suspense>
+                    { routes.map(({ path, component }) => (
+                        <Route exact key={ path } path={ path } component={ component }/>
+                    )) }
+                    <Route exact={ false } key={ '/confirm-registration' } path={ '/confirm-registration' }>
+                        <RegistrationPage/>
+                    </Route>
+                    <Route exact={ false } key={ '/reject-registration' } path={ '/reject-registration' }>
+                        <RegistrationPage/>
+                    </Route>
+                    <Route path={ '*' }>
+                        <Redirect from={ '/' } to={ '/home' }/>
+                    </Route>
                 </Switch>
                 <Footer>
                     <div> Copyright © { new Date().getFullYear() } { AppConstants.appInfo.companyName }.</div>
--- src/content-non-auth.js
+++ src/content-non-auth.js
@@ -1,30 +1,26 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { SingleCard } from './layouts';
 import { LoginForm } from './components';
-import Loader from './components/loader/loader';
 
-const RegistrationPage = lazy(() => import('./pages/registration/registration'));
+
+import RegistrationPage from './pages/registration/registration';
 
 const ContentNonAuth = () => {
     return (
         <Switch>
-            <Suspense fallback={ <Loader/> }>
-                <Route exact path="/login">
-                    <SingleCard title="Вход">
-                        <LoginForm/>
-                    </SingleCard>
-                </Route>
-                <Route exact={ false }  path={ '/confirm-registration' } >
-                    <RegistrationPage />
-                </Route>
-                <Route exact={ false }  path={ '/reject-registration' } >
-                    <RegistrationPage />
-                </Route>
-                <Route>
-                    <Redirect from={ '/' } to={ '/login' } />
-                </Route>
-            </Suspense>
+            <Route exact path="/login">
+                <SingleCard title="Вход">
+                    <LoginForm/>
+                </SingleCard>
+            </Route>
+            <Route exact={ false } path={ '/confirm-registration' }>
+                <RegistrationPage/>
+            </Route>
+            <Route exact={ false } path={ '/reject-registration' }>
+                <RegistrationPage/>
+            </Route>
+            <Redirect to={ '/login' }/>
         </Switch>
     );
 }
