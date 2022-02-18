8ad8f9b0234f9d35cfbd24484bc217e5729cf41c
Вячеслав Леонтьев <70220844+leontev-vyacheslav@users.noreply.github.com>
2022-02-18 19:33:09 +0300

    Update app-api-routes.ts

--- src/constants/app-api-routes.ts
+++ src/constants/app-api-routes.ts
@@ -1,5 +1,5 @@
 export default {
-    host: process.env.NODE_ENV !== 'production' ? 'https://192.168.1.2:5001' : 'https://eta24.ru:15005',
+    host: process.env.NODE_ENV !== 'production' ? 'https://192.168.1.2:5001' : 'xxxx',
     mobileDevice: '/api/mobileDevice',
     timeline: '/api/timeline',
     locationRecord: '/api/locationRecord',
